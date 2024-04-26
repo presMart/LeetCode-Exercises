4415fdd6feb7f65b41eaf4ddd5d7c10df594151e
presMart <presmart@hotmail.com>
2024-04-26 19:04:07 -0400

    April 26 24

--- Medium/divide_two_integers_29.py
+++ Medium/divide_two_integers_29.py
@@ -28,3 +28,25 @@ class Solution:
             return -2147483648
         return cleaned_q
 
+    def divideAlt(self, dividend: int, divisor: int) -> int:
+
+        # NOTE- would using a map for cleaned_q speed up the if check at the end, checking the int's size?
+
+        q_sign = "-" if ((dividend < 0) ^ (divisor < 0)) else ""
+
+        dend = abs(dividend)
+        dor = abs(divisor)
+        q = 0
+        while dend - dor >= 0:
+            x = 0
+            while dend - (dor << 1 << x) >= 0: # while dividend minus (divisor bit shifted left once, divisor bit shifted x times) is greater than or equal to 0
+                x += 1 # while the above is True, increment x by 1, and check if divisor can still go into dividend after being left shifted (x) times
+            q += 1 << x # increment q by the number of times we can double the divisor while it can still fit into dividend
+            dend -= dor << x # decrement dividend by how many times divisor was doubled (ie, by how many times dor was shifted left)
+
+        cleaned_q = int(f'{q_sign}{q}')
+        if cleaned_q >= 2147483647:
+            return 2147483647
+        elif cleaned_q <= -2147483648:
+            return -2147483648
+        return cleaned_q
